462cb08b4bd00e41e59b082310fa6758d7851d9c
Nikos Filippakis <nikolaos.filippakis@cern.ch>
2017-03-01 09:20:38 +0100

    allow multiple defines
    Signed-off-by: Nikos Filippakis <nikolaos.filippakis@cern.ch>

--- check.sh
+++ check.sh
@@ -1,3 +1,4 @@
-#!/bin/sh
-cpp -w -P $2 Ours.hs | cat OursImps.hs $1 - > runnable.hs
+#!/bin/bash
+echo ${@:2}
+cpp -w -P "${@:2}" Ours.hs | cat OursImps.hs $1 - > runnable.hs
 echo gradMain | ghci runnable.hs
